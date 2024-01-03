e7c56273380e6aee0788c9102d31c82d932f3d55
Deepak Kushwaha <88443379+deepakkushwahax@users.noreply.github.com>
2024-01-03 18:30:16 +0530

    Add files via upload

---
+++ SortedMatrix.java
@@ -0,0 +1,27 @@
+//package BinarySearch;
+//
+//public class SortedMatrix {
+//    public static void main(String[] args) {
+//
+//    }
+//    static <colEnd> int[] binarySearch(int[] matrix, int row, int cStart, int cEnd, int target){
+//        while (cStart <= cEnd);
+//        int mid = cStart = (cEnd - cStart) / 2 ;
+//        if (matrix[row][mid] == target){
+//            return new int[]{row, mid};
+//        }
+//        if (matrix[row][mid] < target){
+//
+//        }else {
+//            cStart =
+//        }
+//    }
+//    static int[] search(int[][] matrix, int target) {
+//        int rows = matrix.length;
+//        int cols = matrix[0].length;
+//
+//        if (rows == 1){
+//
+//        }
+//    }
+//}
